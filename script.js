f37a907c4359a2fb9b72d2dc8a5bfa2677aee0f6
Jan Alkhouri <123843918+JanAlkhouri@users.noreply.github.com>
2026-03-24 19:12:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
     let pathname = window.location.pathname;
-    console.log(pathname);
-    if (pathname.endsWith("/index.html")){
+
+    if (pathname === "./Website-Demo/index.html"){
         alert("Welcome to The Band's official website! Get ready to explore our story, our journey, and the music we love.");
-    } else if (pathname.endsWith("/john.html")){
+    } else if (pathname === "./Website-Demo/john.html"){
         alert("Welcome! You are about to get to know John, one of the members of The Band. His story and biography await you!");
-    } else if (pathname.endsWith("/kieran.html")){
+    } else if (pathname === "./Website-Demo/kieran.html"){
         alert("Welcome! You are about to discover Karen, a key member of The Band. Dive in and learn all about her!");
-    } else if (pathname.endsWith("/kimberly.html")){
+    } else if (pathname === "./Website-Demo/kimberly.html"){
         alert("Welcome! You are about to explore Kimberly's world as a member of The Band. Her story is just ahead!");
     }
 });
